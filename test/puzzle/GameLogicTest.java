d2cc7c7ab2b2d34706adaddced2532e46cdcf31a
JonPorsche <jonespor@gmail.com>
2021-06-16 15:56:30 +0200

    JUnit test for method isSolved implemented.

--- test/puzzle/GameLogicTest.java
+++ test/puzzle/GameLogicTest.java
@@ -6,12 +6,15 @@ import org.junit.Test;
 
 public class GameLogicTest {
 
-	GameLogic gameLogic;
+	GameLogic gameLogic = new GameLogic();
+	int[] solved = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16};
+	int[] unsolved1 =  {1,2,3,4,5,6,7,8,9,10,11,12,13,15,14,16};
+	int[] unsolved2 =  {1,2,3,4,5,6,7,8,9,10,11,12,13,14,16,15};
 	
 	@Test
 	public void testIsSolved() {
-		int[] tileNrs = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16};
-		assertEquals(true, gameLogic.isSolved(tileNrs));
+		assertTrue("Solved. The hole and all the tiles are in the solved position.", gameLogic.isSolved(solved));
+		assertFalse("Not solved. Hole is in solved position, but the tiles are not.", gameLogic.isSolved(unsolved1));
+		assertFalse("Not solved. Hole is not in solved position.", gameLogic.isSolved(unsolved2));
 	}
-
 }
